d7c3b95bdd3da25993f3cd0d75a245b80094acbe
hoangdn88 <104883180+hoangdn88@users.noreply.github.com>
2022-06-07 09:07:10 +0700

    sss

--- ExampleAPI/Controllers/WeatherForecastController.cs
+++ ExampleAPI/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace ExampleAPI.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
-        private string connectString = "mongodb://localhost:27077/";
+        private string connectString = "mongodb://localhost:27077,65.0.115.163:27717,65.0.115.163:27018?connect=replicaSet";
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
@@ -21,6 +21,19 @@ namespace ExampleAPI.Controllers
         [HttpGet("hello")]
         public async Task<IActionResult> Hello(CancellationToken cancellationToken)
         {
+            var setting = new MongoClientSettings
+            {
+                Servers = new[]
+                       {
+                          new MongoServerAddress("localhost", 27077),
+                          new MongoServerAddress("65.0.115.163", 27717),
+                          new MongoServerAddress("65.0.115.163", 27018)
+                       },
+                ConnectionMode = ConnectionMode.Automatic,
+                ReplicaSetName = "rs0",
+                WriteConcern = new WriteConcern(WriteConcern.WValue.Parse("3"), wTimeout: TimeSpan.Parse("10"))
+            };
+
             MongoClient client = new MongoClient(connectString);
             var db = client.GetDatabase("FireManufacture");
             var collection = db.GetCollection<DeviceTypeInfo>("DeviceTypeInfo");
